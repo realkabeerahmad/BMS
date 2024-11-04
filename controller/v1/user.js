96f3aad16c49af0951afc4861cb9d47ecdd89de3
realkabeerahmad <kabeerahmadofficial@gmail.com>
2024-11-05 04:39:08 +0500

    Updated user update flow to create dynamic query at run time so that only provided fields can be updated

--- controller/v1/user.js
+++ controller/v1/user.js
@@ -6,7 +6,7 @@ class USER extends LOGGER {
   static queries = {
     CREATE: `INSERT INTO users (user_id, first_name, middle_name, last_name, email, phone, gender, dob, country_code, state_code, city_name, role_id, is_allowed, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`,
     READ: `SELECT * FROM users WHERE user_id = $1`,
-    UPDATE: `UPDATE users SET first_name = $1, middle_name = $2, last_name = $3, email = $4, phone = $5, gender = $6, dob = $7, country_code = $7, state_code = $8, city_name = $9, role_id = $10, is_allowed = $11 WHERE user_id = $12`,
+    UPDATE: `UPDATE users SET first_name = $1,  middle_name = $2, last_name = $3, email = $4,  phone = $5,  gender = $6,  dob = $7, country_code = $8, state_code = $9,  city_name = $10,  role_id = $11, is_allowed = $12 WHERE user_id = $13 RETURNING *;`,
     DELETE: `DELETE FROM users WHERE user_id = $1`,
     UPDATE_PASSWORD: `UPDATE users SET password = $1 WHERE user_id = $2`,
     READ_USER_ID: `SELECT user_id FROM users WHERE user_id = $1`,
@@ -133,31 +133,99 @@ class USER extends LOGGER {
       is_allowed,
     } = req.body.user;
 
+    if (!user_id) {
+      this.WARNING("User ID is required");
+      return res.status(400).send({ error: "User ID is required" });
+    }
+
     try {
-      const result = await this.DB.execute(USER.queries.UPDATE, [
-        first_name,
-        middle_name,
-        last_name,
-        email,
-        phone,
-        gender,
-        dob,
-        country_code,
-        state_code,
-        city_name,
-        role_id,
-        is_allowed,
-        user_id,
-      ]);
-      if (result.affectedRows > 0) {
+      const user = await this.getUserFromDB(user_id, USER.queries.READ);
+
+      if (!user) {
+        this.WARNING("User not found");
+        return res.status(404).send({ error: "User not found" });
+      }
+
+      this.INFO("Updating user:", JSON.stringify(req.body.user));
+
+      // Fields to update and corresponding values
+      const fieldsToUpdate = [];
+      const values = [];
+
+      // Add only fields that are provided in the request body
+      if (first_name !== undefined) {
+        fieldsToUpdate.push("first_name = $1");
+        values.push(first_name);
+      }
+      if (middle_name !== undefined) {
+        fieldsToUpdate.push("middle_name = $" + (values.length + 1));
+        values.push(middle_name);
+      }
+      if (last_name !== undefined) {
+        fieldsToUpdate.push("last_name = $" + (values.length + 1));
+        values.push(last_name);
+      }
+      if (email !== undefined) {
+        fieldsToUpdate.push("email = $" + (values.length + 1));
+        values.push(email);
+      }
+      if (phone !== undefined) {
+        fieldsToUpdate.push("phone = $" + (values.length + 1));
+        values.push(phone);
+      }
+      if (gender !== undefined) {
+        fieldsToUpdate.push("gender = $" + (values.length + 1));
+        values.push(gender);
+      }
+      if (dob !== undefined) {
+        fieldsToUpdate.push("dob = $" + (values.length + 1));
+        values.push(dob);
+      }
+      if (country_code !== undefined) {
+        fieldsToUpdate.push("country_code = $" + (values.length + 1));
+        values.push(country_code);
+      }
+      if (state_code !== undefined) {
+        fieldsToUpdate.push("state_code = $" + (values.length + 1));
+        values.push(state_code);
+      }
+      if (city_name !== undefined) {
+        fieldsToUpdate.push("city_name = $" + (values.length + 1));
+        values.push(city_name);
+      }
+      if (role_id !== undefined) {
+        fieldsToUpdate.push("role_id = $" + (values.length + 1));
+        values.push(role_id);
+      }
+      if (is_allowed !== undefined) {
+        fieldsToUpdate.push("is_allowed = $" + (values.length + 1));
+        values.push(is_allowed);
+      }
+
+      // If no fields to update, return without making a database call
+      if (fieldsToUpdate.length === 0) {
+        this.WARNING("No fields provided for update");
+        return res.status(400).send({ error: "No fields provided for update" });
+      }
+
+      // Add user_id as the last parameter for the WHERE clause
+      values.push(user_id);
+      const query = `UPDATE users SET ${fieldsToUpdate.join(
+        ", "
+      )} WHERE user_id = $${values.length}`;
+      this.DEBUG(query);
+      // Execute the update query
+      const result = await this.DB.CONNECTION.result(query, values);
+
+      if (result.rowCount > 0) {
         this.INFO("User updated successfully");
         res.status(200).send({ message: "User updated successfully" });
       } else {
-        this.WARNING("User not found");
-        res.status(404).send({ message: "User not found" });
+        this.WARNING("Failed to update user - no changes applied");
+        res.status(304).send({ message: "No changes made to the user" });
       }
     } catch (error) {
-      this.ERROR("Error updating user:", error);
+      this.ERROR("Error updating user: " + error);
       res.status(500).send({ error: "Failed to update user" });
     }
   };
@@ -168,10 +236,10 @@ class USER extends LOGGER {
 
     try {
       const hashedPassword = await this.hashPassword(newPassword);
-      const result = await this.DB.execute(USER.queries.UPDATE_PASSWORD, [
-        hashedPassword,
-        user_id,
-      ]);
+      const result = await this.DB.CONNECTION.oneOrNone(
+        USER.queries.UPDATE_PASSWORD,
+        [hashedPassword, user_id]
+      );
 
       if (result.affectedRows > 0) {
         this.INFO("Password updated successfully");
