ef2305f660b3a301d960140f1668936898ce67b9
realkabeerahmad <kabeerahmadofficial@gmail.com>
2024-11-05 03:35:40 +0500

    Optimized user creation flow and getUser API

--- controller/v1/user.js
+++ controller/v1/user.js
@@ -6,9 +6,10 @@ class USER extends LOGGER {
   static queries = {
     CREATE: `INSERT INTO users (user_id, first_name, middle_name, last_name, email, phone, gender, dob, country_code, state_code, city_name, role_id, is_allowed, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`,
     READ: `SELECT * FROM users WHERE user_id = $1`,
-    UPDATE: `UPDATE users SET first_name = ?, middle_name = ?, last_name = ?, email = ?, phone = ?, gender = ?, dob = ?, country_code = ?, state_code = ?, city_name = ?, role_id = ?, is_allowed = ? WHERE user_id = ?`,
-    DELETE: `DELETE FROM users WHERE user_id = ?`,
-    UPDATE_PASSWORD: `UPDATE users SET password = ? WHERE user_id = ?`,
+    UPDATE: `UPDATE users SET first_name = $1, middle_name = $2, last_name = $3, email = $4, phone = $5, gender = $6, dob = $7, country_code = $7, state_code = $8, city_name = $9, role_id = $10, is_allowed = $11 WHERE user_id = $12`,
+    DELETE: `DELETE FROM users WHERE user_id = $1`,
+    UPDATE_PASSWORD: `UPDATE users SET password = $1 WHERE user_id = $2`,
+    READ_USER_ID: `SELECT user_id FROM users WHERE user_id = $1`,
   };
 
   constructor() {
@@ -25,14 +26,20 @@ class USER extends LOGGER {
     const charset =
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}|:<>?-=[];,./";
     let password = "";
-
+    let charSetLength = charset.length;
     for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * charset.length);
+      const randomIndex = Math.floor(Math.random() * charSetLength);
       password += charset[randomIndex];
     }
 
     return password;
   };
+
+  getUserFromDB = async (user_id, query) => {
+    const user = await this.DB.CONNECTION.oneOrNone(query, [user_id]);
+    return user;
+  };
+
   // Create a new user with encrypted password
   createUser = async (req, res) => {
     const {
@@ -50,51 +57,41 @@ class USER extends LOGGER {
       role_id,
       is_allowed,
     } = req.body.user;
-
-    this.DEBUG(JSON.stringify(req.body.user));
-
-    try {
-      const password = this.generateOneTimePassword(8);
-      if (!password) {
-        this.ERROR("Password is required");
-        return res.status(400).send({ error: "Password is required" });
+    const user = await this.getUserFromDB(user_id, USER.queries.READ_USER_ID);
+    if (!user) {
+      try {
+        const password = this.generateOneTimePassword(8);
+        // Await the hashing of the password
+        const hashedPassword = await this.hashPassword(password);
+        this.DEBUG("Query used to create user: " + USER.queries.CREATE);
+        // Insert user into the database
+        const result = await this.DB.CONNECTION.oneOrNone(USER.queries.CREATE, [
+          user_id,
+          first_name,
+          middle_name,
+          last_name,
+          email,
+          phone,
+          gender,
+          dob,
+          country_code,
+          state_code,
+          city_name,
+          role_id,
+          is_allowed || "Y",
+          hashedPassword,
+        ]);
+        this.INFO("User created successfully");
+        res
+          .status(201)
+          .send({ message: "User created successfully", user: result });
+      } catch (error) {
+        this.ERROR("Error creating user: " + error);
+        res.status(500).send({ error: "Failed to create user" });
       }
-
-      // Await the hashing of the password
-      const hashedPassword = await this.hashPassword(password);
-      this.DEBUG("Password generated: " + password);
-      this.DEBUG("Hashed Password: " + hashedPassword);
-      this.DEBUG("Query used to create user: " + USER.queries.CREATE);
-
-      if (!this.DB.CONNECTION) throw new Error("ISSUE IN DB CONNECTION");
-
-      // Insert user into the database
-      const result = await this.DB.CONNECTION.oneOrNone(USER.queries.CREATE, [
-        user_id,
-        first_name,
-        middle_name,
-        last_name,
-        email,
-        phone,
-        gender,
-        dob,
-        country_code,
-        state_code,
-        city_name,
-        role_id,
-        is_allowed || "Y",
-        hashedPassword, // Ensure hashedPassword is a string
-      ]);
-
-      this.INFO("User created successfully");
-      res
-        .status(201)
-        .send({ message: "User created successfully", user: result });
-    } catch (error) {
-      this.ERROR("Error creating user: " + error);
-      res.status(500).send({ error: "Failed to create user" });
-    } finally {
-      this.INFO("API Request fulfilled".toUpperCase());
+    } else {
+      this.DEBUG(JSON.stringify(req.body.user));
+      res.status(400).json({ error: "User Already exist" });
     }
   };
 
@@ -102,10 +99,8 @@ class USER extends LOGGER {
   getUser = async (req, res) => {
     const { user_id } = req.params;
     this.DEBUG("USER ID GOT IN REQUEST IS: " + user_id);
+    const user = await this.getUserFromDB(user_id, USER.queries.READ);
     try {
-      const user = await this.DB.CONNECTION.oneOrNone(USER.queries.READ, [
-        user_id,
-      ]);
       if (user) {
         delete user.password; // Remove password from response for security
         this.INFO("User retrieved successfully");
